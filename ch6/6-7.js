d6aa0c6fbeba4bb0ffebf8913d4b55ecdc955837
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-07-17 00:08:01 +0900

    변수 이름

---
+++ ch6/6-7.js
@@ -0,0 +1,13 @@
+// let a = height * width;
+
+// const cpyNm = '애플';
+
+// let tpHd = '제목없음';
+// let result = `<h1>${tpHd}</h1>`;
+
+let area = width * height;
+
+const companyName = "애플";
+
+let title = "제목없음";
+let result = `<h1>${title}</h1>`;
