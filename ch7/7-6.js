41f45f45f3c01cb0f873b1a3658ebce9ffd4dc6d
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-07-24 00:09:12 +0900

    클래스 인라인 리팩토링

---
+++ ch7/7-6.js
@@ -0,0 +1,83 @@
+// export class TrackingInformation {
+//   #shippingCompany;
+//   #trackingNumber;
+//   constructor(trackingNumber, shippingCompany) {
+//     this.#trackingNumber = trackingNumber;
+//     this.#shippingCompany = shippingCompany;
+//   }
+
+//   get shippingCompany() {
+//     return this.#shippingCompany;
+//   }
+
+//   set shippingCompany(arg) {
+//     this.#shippingCompany = arg;
+//   }
+
+//   get trackingNumber() {
+//     return this.#trackingNumber;
+//   }
+
+//   set trackingNumber(arg) {
+//     this.#trackingNumber = arg;
+//   }
+
+//   get display() {
+//     return `${this.shippingCompany}: ${this.trackingNumber}`;
+//   }
+// }
+
+// export class Shipment {
+//   #trackingInformation;
+//   constructor(trackingInformation) {
+//     this.#trackingInformation = trackingInformation;
+//   }
+
+//   get trackingInfo() {
+//     return this.#trackingInformation.display;
+//   }
+
+//   get trackingInformation() {
+//     return this.#trackingInformation;
+//   }
+
+//   set trackingInformation(trackingInformation) {
+//     this.#trackingInformation = trackingInformation;
+//   }
+// }
+
+export class Shipment {
+  #shippingCompany;
+  #trackingNumber;
+  constructor(trackingNumber, shippingCompany) {
+    this.#trackingNumber = trackingNumber;
+    this.#shippingCompany = shippingCompany;
+  }
+
+  get shippingCompany() {
+    return this.#shippingCompany;
+  }
+
+  set shippingCompany(arg) {
+    this.#shippingCompany = arg;
+  }
+
+  get trackingNumber() {
+    return this.#trackingNumber;
+  }
+
+  set trackingNumber(arg) {
+    this.#trackingNumber = arg;
+  }
+
+  get trackingInfo() {
+    return `${this.shippingCompany}: ${this.trackingNumber}`;
+  }
+}
+
+// const shipment = new Shipment(new TrackingInformation(999, 'Maersk'));
+const shipment = new Shipment(999, "Maersk");
+console.log(shipment.trackingInfo);
+
+shipment.shippingCompany = "COSCO";
+console.log(shipment.trackingInfo);
