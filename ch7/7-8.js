6bbb3853c8f038d7c02061f6259d0311a7e2ff0a
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-07-24 23:45:53 +0900

    중개자 제거

---
+++ ch7/7-8.js
@@ -0,0 +1,86 @@
+// class Person {
+//   #name;
+//   #department;
+//   constructor(name, department) {
+//     this.#name = name;
+//     this.#department = department;
+//   }
+
+//   get name() {
+//     return this.#name;
+//   }
+
+//   get department() {
+//     return this.#department;
+//   }
+
+//   set department(arg) {
+//     this.#department = arg;
+//   }
+// }
+
+// export class Department {
+//   #manager;
+//   #chargeCode;
+//   constructor(manager, chargeCode) {
+//     this.#manager = manager;
+//     this.#chargeCode = chargeCode;
+//   }
+
+//   get chargeCode() {
+//     return this.#chargeCode;
+//   }
+
+//   set chargeCode(arg) {
+//     this.#chargeCode = arg;
+//   }
+
+//   get manager() {
+//     return this.#manager;
+//   }
+
+//   set manager(arg) {
+//     this.#manager = arg;
+//   }
+// }
+
+// const person = new Person('Tom', new Department('aManager', '999'));
+// console.log(person.name);
+// console.log(person.department.manager);
+// console.log(person.department.chargeCode);
+
+class Person {
+  #name;
+  #manager;
+  #chargeCode;
+  constructor(name, manager, chargeCode) {
+    this.#name = name;
+    this.#manager = manager;
+    this.#chargeCode = chargeCode;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get chargeCode() {
+    return this.#chargeCode;
+  }
+
+  set chargeCode(arg) {
+    this.#chargeCode = arg;
+  }
+
+  get manager() {
+    return this.#manager;
+  }
+
+  set manager(arg) {
+    this.#manager = arg;
+  }
+}
+
+const person = new Person("Tom", "aManager", "999");
+console.log(person.name);
+console.log(person.manager);
+console.log(person.chargeCode);
