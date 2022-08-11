93df0227e07d7404988e984b34d75482b4072f01
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-08-11 23:47:57 +0900

    질의 함수를 매개변수로 바꾸기

---
+++ ch11/11-6.js
@@ -0,0 +1,16 @@
+/* 
+    질의 함수를 매개변수로 바꾸기
+*/
+
+targetTemperature(aPlan, thermostat.currentTemperature);
+
+// 다른모듈에 있는 함수라고 가정
+// function targetTemperature(aPlan) {
+//   currentTemperature = thermostat.currentTemperature;
+//   // ...
+// }
+
+function targetTemperature(plan, currentTemperature;) {
+  currentTemperature = currentTemperature;
+  // ...
+}
