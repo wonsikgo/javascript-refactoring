e8c3fd9341656e91b96c3ca0a48c0056388b8919
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-08-18 23:48:04 +0900

    생성자 본문 올리기

---
+++ ch12/12-3.js
@@ -0,0 +1,34 @@
+/* 
+    생성자 본문 올리기
+*/
+class Party {
+  #name;
+  constructor(name) {
+    this.#name = name;
+  }
+}
+
+class Employee extends Party {
+  // #name;
+  #id;
+  #monthlyCost;
+  constructor(name, id, monthlyCost) {
+    super(name);
+    // this.#name = name;
+    this.#id = id;
+    this.#monthlyCost = monthlyCost;
+  }
+}
+
+class Department extends Party {
+  // #name;
+  #staff;
+  constructor(name, staff) {
+    super(name);
+    // this.#name = name;
+    this.#staff = staff;
+  }
+}
+
+const ellie = new Employee("엘리", 123, 13);
+const department = new Department("개발부서", ellie);
