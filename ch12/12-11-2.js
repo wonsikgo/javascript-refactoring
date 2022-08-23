c84aeab10498e85e3ac548959545e548948d28e8
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-08-23 23:39:46 +0900

    최종 챌린지

--- ch12/12-11-2.js
+++ ch12/12-11-2.js
@@ -18,14 +18,43 @@ class CatalogItem {
   }
 }
 
-class Scroll extends CatalogItem {
-  constructor(id, title, tags, dataLastCleaned) {
-    super(id, title, tags);
+// class Scroll extends CatalogItem {
+//   constructor(id, title, tags, dataLastCleaned) {
+//     super(id, title, tags);
+//     this._lastCleaned = dataLastCleaned;
+//   }
+
+//   needsCleaning(targetDate) {
+//     const threshold = this.hasTag('revered') ? 700 : 1500;
+
+//     return this.daysSinceLastCleaning(targetDate) > threshold;
+//   }
+
+//   daysSinceLastCleaning(targetDate) {
+//     return this._lastCleaned.until(targetDate, ChronoUnit.DAYS);
+//   }
+// }
+
+class Scroll {
+  constructor(id, dataLastCleaned, catelogItem) {
+    this._id = id;
     this._lastCleaned = dataLastCleaned;
   }
 
+  get id() {
+    return this.id;
+  }
+
+  get title() {
+    return this._catalogItem.title;
+  }
+
+  hasTag(aString) {
+    return this._catalogItem.tags.hasTag(aString);
+  }
+
   needsCleaning(targetDate) {
-    const threshold = this.hasTag('revered') ? 700 : 1500;
+    const threshold = this.hasTag("revered") ? 700 : 1500;
 
     return this.daysSinceLastCleaning(targetDate) > threshold;
   }
@@ -34,3 +63,13 @@ class Scroll extends CatalogItem {
     return this._lastCleaned.until(targetDate, ChronoUnit.DAYS);
   }
 }
+
+const scrolls = aDocument.map(
+  (record) =>
+    new Scroll(
+      record.id,
+      LocalDate.parse(record.lastCleaned),
+      record.catalogData.id,
+      catalog
+    )
+);
