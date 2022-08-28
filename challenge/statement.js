9567a6b5f81769c5bff7a13fe23f979fa7c290d2
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-08-28 23:15:28 +0900

    html 출력 버전 함수 추가

--- challenge/statement.js
+++ challenge/statement.js
@@ -11,11 +11,29 @@
 import { createStatement } from "./create_statement.js";
 
 export function statement(invoice, plays) {
-  const statement = createStatement(invoice, plays);
-  return renderPlainText(statement);
+  return renderPlainText(createStatement(invoice, plays));
 }
 
-function renderPlainText(statement, plays) {
+export function htmlStatement(invoice, plays) {
+  return renderHTML(createStatement(invoice, plays));
+}
+
+function renderHTML(statement) {
+  let result = `<h1>청구 내역 (고객명: ${statement.customer})</h1>\n`;
+  result += `<table>\n`;
+  result += `<tr><th>play</th><th>석</th></tr>\n`;
+
+  for (let perf of statement.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td> `;
+    result += `<td>${usd(perf.amount / 100)}</td></tr>`;
+  }
+  reuslt += `</table>\n`;
+  result += `<p>총액: ${usd(statement.totalAmount / 100)}</p>\n`;
+  result += `<p>적립 포인트: ${statement.totalCredtis}점</p>\n`;
+  return result;
+}
+
+function renderPlainText(statement) {
   let result = `청구 내역 (고객명: ${statement.customer})\n`;
 
   for (let perf of statement.performances) {
