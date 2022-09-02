ec1820f2a99c0d306fecb64ca100ae63b30be2d1
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-09-02 23:48:23 +0900

    의존성 주입

--- ch6-testable/test/61.test.js
+++ ch6-testable/test/61.test.js
@@ -1,5 +1,33 @@
 import { printOwing } from "../6-1";
 
+class Console {
+  #content = "";
+  constructor() {}
+  log(message) {
+    this.#content += `${message}\n`;
+  }
+  get content() {
+    return this.#content;
+  }
+}
+
+class Clock {
+  constructor() {}
+  get today() {
+    return {
+      getFullYear() {
+        return 2022;
+      },
+      getMonth() {
+        return 0;
+      },
+      getDate() {
+        return 21;
+      },
+    };
+  }
+}
+
 describe("printOwing", () => {
   it("should print owing", () => {
     const invoice = {
@@ -13,6 +41,10 @@ describe("printOwing", () => {
       "name: 엘리\n" +
       "amount: 7\n" +
       "due: 2/20/2022\n";
+
+    const console = new Console();
+    const clock = new Clock();
+    printOwing(invoice, console, clock);
     expect(console.content).toBe(expected);
   });
 });
